03de25fbf5e4b4be269766db6c18de4946d2f10c
Dovias <dovias@zohomail.eu>
2024-02-21 19:12:07 +0200

    refactor code to be more extensible and efficient

--- src/main/java/me/dovias/sourceryacademy/Application.java
+++ src/main/java/me/dovias/sourceryacademy/Application.java
@@ -8,49 +8,43 @@ import java.util.function.Function;
 
 public class Application {
 
-    public static <T> void execute(Integer number, Consumer<T> executor, List<Function<Integer, T>> functions, Function<Integer, T> fallback) {
+    public static <T> void execute(Integer number, Consumer<T> executor, List<Function<Integer, T>> functions, Function<Integer, T> callback, Function<Integer, T> fallback) {
+        boolean shouldFallback = true;
         for (Function<Integer, T> function : functions) {
             T object = function.apply(number);
             if (object != null) {
                 executor.accept(object);
-                return;
+                shouldFallback = false;
             }
         }
-        executor.accept(fallback.apply(number));
+        if (shouldFallback) {
+            executor.accept(fallback.apply(number));
+        }
+        executor.accept(callback.apply(number));
     }
 
-    public static <T> void executeFor(int length, Consumer<T> executor, List<Function<Integer, T>> functions, Function<Integer, T> fallback) {
+    public static <T> void executeFor(int length, Consumer<T> executor, List<Function<Integer, T>> functions, Function<Integer, T> callback, Function<Integer, T> fallback) {
         for (int number = 1; number <= length; number++) {
-            execute(number, executor, functions, fallback);
+            execute(number, executor, functions, callback, fallback);
         }
     }
 
     private static List<Function<Integer, String>> getFunctions() {
         List<Function<Integer, String>> functions = new ArrayList<>();
-
-        Function<Integer, String> fizz = (integer) -> integer % 3 == 0 ? "Fizz" : null;
-        Function<Integer, String> buzz = (integer) -> integer % 5 == 0 ? "Buzz" : null;
-        Function<Integer, String> jazz = (integer) -> integer % 7 == 0 ? "Jazz" : null;
-        Function<Integer, String> fizzBuzz = new CombinedStringFunction<>(fizz, buzz);
-
-        functions.add(new CombinedStringFunction<>(fizzBuzz, jazz));
-        functions.add(new CombinedStringFunction<>(fizz, jazz));
-        functions.add(new CombinedStringFunction<>(buzz, jazz));
-        functions.add(fizzBuzz);
-        functions.add(fizz);
-        functions.add(buzz);
-        functions.add(jazz);
+        functions.add((integer) -> integer % 3 == 0 ? "Fizz" : null);
+        functions.add((integer) -> integer % 5 == 0 ? "Buzz" : null);
+        functions.add((integer) -> integer % 7 == 0 ? "Jazz" : null);
         return functions;
     }
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            return;
-        }
         int length;
         try {
             length = Integer.parseUnsignedInt(args[0]);
-        } catch (NumberFormatException exception) {
+        } catch (Exception exception) {
+            return;
+        }
+        if (length < 1) {
             return;
         }
 
@@ -61,7 +55,7 @@ public class Application {
         (not stating that its very performant with all those heap function objects)
         */
         StringBuilder builder = new StringBuilder();
-        executeFor(length, (string) -> builder.append(string).append(' '), getFunctions(), Objects::toString);
+        executeFor(length, builder::append, getFunctions(), (integer) -> " ", Objects::toString);
         System.out.println(builder.substring(0, builder.length() - 1));
     }
 }
